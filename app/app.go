97c7d759587f76a5738d2af1bc3e437ab5077785
Pablo Garcia <pablo.garcia366@gmail.com>
2022-07-03 10:30:17 -0300

    feat: create account service

--- app/app.go
+++ app/app.go
@@ -1,11 +1,14 @@
 package app
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
 	"github.com/pablogugarcia/banking/domain"
 	"github.com/pablogugarcia/banking/service"
@@ -32,13 +35,40 @@ func Start() {
 	godotenv.Load()
 	sanityCheck()
 	router := mux.NewRouter()
+	dbClient := getDbClient()
 
+	customerRepositoryDb := domain.NewCustomerRepositoryDb(dbClient)
 	// wiring
 	// ch := CustomersHandlers{service: service.NewCustomerService(domain.NewCustomerRepositoryStub())}
-	ch := CustomersHandlers{service: service.NewCustomerService(domain.NewCustomerRepositoryDb())}
+	ch := CustomersHandlers{service: service.NewCustomerService(customerRepositoryDb)}
 
 	router.HandleFunc("/customers", ch.getAllCoustumers).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer).Methods(http.MethodGet)
 
 	log.Fatal(http.ListenAndServe("localhost:8000", router))
+}
+
+func getDbClient() *sqlx.DB {
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbAddr := os.Getenv("DB_ADDR")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dataSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		dbUser,
+		dbPass,
+		dbAddr,
+		dbPort,
+		dbName)
+
+	client, err := sqlx.Open("mysql", dataSource)
+	if err != nil {
+		panic(err)
+	}
+	// See "Important settings" section.
+	client.SetConnMaxLifetime(time.Minute * 3)
+	client.SetMaxOpenConns(10)
+	client.SetMaxIdleConns(10)
+	return client
 }
--- domain/customerRepositoryDb.go
+++ domain/customerRepositoryDb.go
@@ -2,9 +2,6 @@ package domain
 
 import (
 	"database/sql"
-	"fmt"
-	"os"
-	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
@@ -53,27 +50,7 @@ func (d CustomerRepositoryDb) ById(id string) (*Customer, *errs.AppErr) {
 	return &c, nil
 }
 
-func NewCustomerRepositoryDb() CustomerRepositoryDb {
-	dbUser := os.Getenv("DB_USER")
-	dbPass := os.Getenv("DB_PASSWORD")
-	dbAddr := os.Getenv("DB_ADDR")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
+func NewCustomerRepositoryDb(dbClient *sqlx.DB) CustomerRepositoryDb {
 
-	dataSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
-		dbUser,
-		dbPass,
-		dbAddr,
-		dbPort,
-		dbName)
-
-	client, err := sqlx.Open("mysql", dataSource)
-	if err != nil {
-		panic(err)
-	}
-	// See "Important settings" section.
-	client.SetConnMaxLifetime(time.Minute * 3)
-	client.SetMaxOpenConns(10)
-	client.SetMaxIdleConns(10)
-	return CustomerRepositoryDb{client}
+	return CustomerRepositoryDb{dbClient}
 }
